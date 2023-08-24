cd091d2a70b067100b026b4d99b40939c179fd41
Bastian Müller <bastian@turbolent.com>
2023-08-24 08:50:34 -0700

    improve navigation

--- pages/index.js
+++ pages/index.js
@@ -25,8 +25,7 @@ export default function Home() {
           <a href="https://developers.flow.com/cadence/">Learn</a>
           <a href="https://play.flow.com/">Playground</a>
           <a href="https://developers.flow.com/cadence/language">Documentation</a>
-          <a href="/community">Community</a>
-          <a href="/security">Security</a>
+          <a href="https://flow.com/flow-responsible-disclosure">Security</a>
           <a href="https://github.com/onflow/cadence" title="GitHub"><FaGithub/></a>
           <a href="https://discord.com/invite/J6fFnh2xx6" title="Discord"><FaDiscord/></a>
         </div>

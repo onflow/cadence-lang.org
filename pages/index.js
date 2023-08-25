c6addf9d33d21964b8e3fe1e69c0b87ccbe3dc0f
Bastian Müller <bastian@turbolent.com>
2023-08-25 10:46:28 -0700

    add Open Graph tags

--- pages/index.js
+++ pages/index.js
@@ -56,6 +56,9 @@ export default function Home() {
         <meta name="description" content="The resource-oriented smart contract programming language" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="Cadence" />
+        <meta property="og:description" content="The resource-oriented smart contract programming language" />
+        <meta property="og:url" content="https://cadence-lang.org" />
       </Head>
 
       <nav>

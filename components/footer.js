b4782d2694adf114a572fb8f28e013c66c7356ea
Bastian Müller <bastian@turbolent.com>
2023-08-28 14:09:47 -0700

    BSD -> Apache

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
         <div className="content-wrapper">
             <div className="license">
             Except as otherwise noted, this site is licensed under a Creative Commons Attribution 4.0 International License,
-            and code samples are licensed under the 3-Clause BSD License.
+            and code samples are licensed under the Apache License, Version 2.0.
             </div>
         </div>
     </footer>;

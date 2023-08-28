f52e6bc397d457823453b838d73769621d5d8adc
Bastian Müller <bastian@turbolent.com>
2023-08-28 16:21:30 -0700

    match styling of community page

--- pages/learn.js
+++ pages/learn.js
@@ -1,51 +1,71 @@
 
-import YouTube from 'react-youtube';
+import Head from 'next/head'
+import YouTube from 'react-youtube'
 
 export default function Learn() {
-    return <main>
-        <div className="content-wrapper">
-            <h1>Learn Cadence</h1>
-            <p>
-                Learning Cadence is easy. Get started:
-            </p>
-            <ul>
-                <li>
-                    <span className='icon'>🏃‍♀️</span> Walk through the <a href="https://developers.flow.com/cadence/tutorial/first-steps">tutorial</a></li>
-                <li>
-                    <span className='icon'>🛝</span> Try Cadence in the <a href="https://play.flow.com/">Playground</a>
-                </li>
-                <li>
-                    <span className='icon'>📕</span> Read the <a href="https://developers.flow.com/cadence/language">documentation</a>
-                </li>
-                <li>
-                    <span className='icon'>🎓</span> Take the <a href="https://academy.ecdao.org/en">Emerald academy Beginner Cadence course</a>
-                </li>
-                <li>
-                    <span className='icon'>🦍</span> Read how the <a href="https://flow.com/post/implementing-the-bored-ape-yacht-club-smart-contract-in-cadence">Bored Ape Yacht Club contract was implemented</a>
-                </li>
-                <li>
-                    <span className='icon'>📺</span> Watch some videos
-                </li>
-            </ul>
-        </div>
-        <div className="videos">
-          <div className="content-wrapper">
+    return <>
+        <Head>
+            <title>Cadence: Learn</title>
+        </Head>
 
-            <div className='videoSet'>
-              <YouTube
-                videoId="ilJIvoD_qNI"
-              />
-              <YouTube
-                videoId="iVevnipJbHo"
-                opts={{
-                  playerVars: {
-                    list: "PLvcQxi9WyGdF32YuZABVTx-t3-FsBNCN2",
-                    listType: 'playlist'
-                  }
-                }}
-              />
+        <main>
+            <div className="content-wrapper">
+                <h1>Learn Cadence</h1>
+                <p>
+                    Learning Cadence is easy. Get started:
+                </p>
+                <ul>
+                    <li>
+                        <strong>
+                            <span className='icon'>🏃‍♀️</span> Walk through the <a href="https://developers.flow.com/cadence/tutorial/first-steps">tutorial</a>
+                        </strong>
+                    </li>
+                    <li>
+                        <strong>
+                            <span className='icon'>🛝</span> Try Cadence in the <a href="https://play.flow.com/">Playground</a>
+                        </strong>
+                    </li>
+                    <li>
+                        <strong>
+                            <span className='icon'>📕</span> Read the <a href="https://developers.flow.com/cadence/language">documentation</a>
+                        </strong>
+                    </li>
+                    <li>
+                        <strong>
+                            <span className='icon'>🎓</span> Take the <a href="https://academy.ecdao.org/en">Emerald academy Beginner Cadence course</a>
+                        </strong>
+                    </li>
+                    <li>
+                        <strong>
+                            <span className='icon'>🦍</span> Read how the <a href="https://flow.com/post/implementing-the-bored-ape-yacht-club-smart-contract-in-cadence">Bored Ape Yacht Club contract was implemented</a>
+                        </strong>
+                    </li>
+                    <li>
+                        <strong>
+                            <span className='icon'>📺</span> Watch some videos
+                        </strong>
+                    </li>
+                </ul>
             </div>
-          </div>
-        </div>
-    </main>
+            <div className="videos">
+            <div className="content-wrapper">
+
+                <div className='videoSet'>
+                <YouTube
+                    videoId="ilJIvoD_qNI"
+                />
+                <YouTube
+                    videoId="iVevnipJbHo"
+                    opts={{
+                    playerVars: {
+                        list: "PLvcQxi9WyGdF32YuZABVTx-t3-FsBNCN2",
+                        listType: 'playlist'
+                    }
+                    }}
+                />
+                </div>
+            </div>
+            </div>
+        </main>
+    </>;
 }

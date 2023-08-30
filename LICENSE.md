b1e5fd60ae4bcd104048c0bbec754f1404d9b05a
Bastian Müller <bastian@turbolent.com>
2023-08-30 11:14:53 -0700

    improve license

---
+++ LICENSE.md
@@ -0,0 +1,2 @@
+The source code of this site is licensed under the Apache License, Version 2.0 (LICENSE.txt).
+Content is licensed under the Creative Commons Attribution 4.0 International License (CC-BY-4.0.txt).
--- components/footer.js
+++ components/footer.js
@@ -4,8 +4,8 @@ export default function Footer() {
         <div className="content-wrapper">
             <div className="feature">
                 <div className="license">
-                    Except as otherwise noted, this site is licensed under a Creative Commons Attribution 4.0 International License,
-                    and code samples are licensed under the Apache License, Version 2.0.
+                    The source code of this site is licensed under the Apache License, Version 2.0.
+                    Content is licensed under the Creative Commons Attribution 4.0 International License.
                 </div>
                 <div>
                     Got suggestions for this site? <br/>

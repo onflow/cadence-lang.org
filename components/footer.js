eb2a3de69b29d2eb84576a17e815e0c96d472b1a
Bastian Müller <bastian@turbolent.com>
2023-08-30 11:07:13 -0700

    add link to website repo

--- components/footer.js
+++ components/footer.js
@@ -2,9 +2,15 @@
 export default function Footer() {
     return <footer>
         <div className="content-wrapper">
-            <div className="license">
-            Except as otherwise noted, this site is licensed under a Creative Commons Attribution 4.0 International License,
-            and code samples are licensed under the Apache License, Version 2.0.
+            <div className="feature">
+                <div className="license">
+                    Except as otherwise noted, this site is licensed under a Creative Commons Attribution 4.0 International License,
+                    and code samples are licensed under the Apache License, Version 2.0.
+                </div>
+                <div>
+                    Got suggestions for this site? <br/>
+                    It's <a href="https://github.com/onflow/cadence-lang.org">open-source</a>!
+                </div>
             </div>
         </div>
     </footer>;

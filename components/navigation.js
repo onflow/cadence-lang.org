b5098134bf56a6a78c9b42b61d7d9deb8c8ea4af
Bastian Müller <bastian@turbolent.com>
2023-09-11 18:00:49 -0700

    open new page for external links

--- components/navigation.js
+++ components/navigation.js
@@ -12,19 +12,48 @@ export default function Navigation() {
                 className="logo"
                 href="/"
             >Cadence</Link>
+
             <Link
                 className={pathname === '/learn' ? 'active' : ''}
                 href="/learn"
             >Learn</Link>
-            <a href="https://play.flow.com/">Playground</a>
+
+            <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://play.flow.com/"
+            >Playground</a>
+
             <Link
                 className={pathname === '/community' ? 'active' : ''}
                 href="/community"
             >Community</Link>
-            <a href="https://developers.flow.com/cadence/language">Documentation</a>
-            <a href="https://flow.com/flow-responsible-disclosure">Security</a>
-            <a href="https://github.com/onflow/cadence" title="GitHub"><FaGithub/></a>
-            <a href="https://discord.com/invite/J6fFnh2xx6" title="Discord"><FaDiscord/></a>
+
+            <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://developers.flow.com/cadence/language"
+            >Documentation</a>
+
+            <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://flow.com/flow-responsible-disclosure"
+            >Security</a>
+
+            <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/onflow/cadence"
+                title="GitHub"
+            ><FaGithub/></a>
+
+            <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://discord.com/invite/J6fFnh2xx6"
+                title="Discord"
+            ><FaDiscord/></a>
         </div>
     </nav>;
 }
